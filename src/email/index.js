2ad840e7b362989a3d70bd4c1daa743750694c1e
Souvik <souvikde.ns@gmail.com>
2020-11-05 01:26:09 +0530

    email layer

---
+++ src/email/index.js
@@ -0,0 +1,20 @@
+
+const sendEventInvite = async () => {
+    /**
+     * Will send email invites to all the users 
+     * who have RSVP the event.
+     */
+}
+
+//This function is for future idea
+const sendEmailforEventCreation = async () => {
+    /**
+     * Will send email notification when a new 
+     * event will be created.
+     */
+}
+
+module.exports = {
+    sendEventInvite,
+    sendEmailforEventCreation
+}
---
+++ src/email/mail.service.js
@@ -0,0 +1,28 @@
+const mailjetClient = require('node-mailjet');
+const { mailjet } = require('../config');
+
+class Mail {
+    constructor() {
+        this.mailjet = mailjetClient.connect(mailjet.api_key, mailjet.secret_key);
+    }
+
+    async sendMail({ subject, text, html, recipientsLists }) {
+        let to = recipientsLists.map(el => {
+            return { Email: el }
+        });
+        try {
+            let res = await this.mailjet.post('send').request({
+                FromEmail: mailjet.mail,
+                FromName: "TechPhantoms",
+                Subject: subject,
+                "Text-part": text,
+                "Html-part": html,
+                Recipients: to
+            })
+
+            return res
+        } catch (error) {
+            throw error
+        }
+    }
+}
