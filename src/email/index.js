ad91b840ea03a3a457b1c21216b86fb17c3ab0a1
Souvik <souvikde.ns@gmail.com>
2020-11-05 01:44:53 +0530

    creating templates

--- src/email/index.js
+++ src/email/index.js
@@ -1,6 +1,6 @@
 const emailClient = require('./mail.service');
 
-const sendEventInvite = async (users,eventDetails) => {
+exports.sendEventInvite = async (users, eventDetails) => {
     /**
      * Will send email invites to all the users 
      * who have RSVP the event.
@@ -8,14 +8,24 @@ const sendEventInvite = async (users,eventDetails) => {
 }
 
 //This function is for future idea
-const sendEmailforEventCreation = async () => {
+exports.sendEmailforEventCreation = async () => {
     /**
      * Will send email notification when a new 
      * event will be created.
      */
 }
 
-module.exports = {
-    sendEventInvite,
-    sendEmailforEventCreation
+const getEventInviteTemplate = ({ name, description, eventImage, date, time, mlsa_event, reg_link }) => {
+    // we can create a beautifull html template
+    let template = require('./template').inviteTemplate
+
+    template = template.replace(/{{name}}/, name);
+    template = template.replace(/{{description}}/, description);
+    template = template.replace(/{{eventImage}}/, eventImage);
+    template = template.replace(/{{date}}/, date);
+    template = template.replace(/{{time}}/, time);
+    template = template.replace(/{{mlas_event}}/,mlsa_event);
+    template = template.replace(/{{reg_link}}/, reg_link);
+
+    return template;
 }
