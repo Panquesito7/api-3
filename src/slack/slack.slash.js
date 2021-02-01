5e0107534e6657ba94236345b02a070de773af19
Souvik <souvikde.ns@gmail.com>
2021-02-01 18:44:41 +0530

    Update slack.slash.js

--- src/slack/slack.slash.js
+++ src/slack/slack.slash.js
@@ -1,10 +1,22 @@
 const { Bank } = require('../models/bank');
+const slack = require('slack');
 
 const mine = async (req, res) => {
     if (req.body.channel_name !== "bot-cmds") {
         return res.send("> :bow: sorry this command only works in <#C01LB9BKRB8|bot-cmds>");
     }
-    return res.send("Currently Under development")
+
+    let coins = coinGenerator();
+
+    return res.send(`> :tada: Found _${coins} tp_ :tada: 
+    > <@${req.body.user_id}|user> _${coins}_ tp is being added to your account
+    `)
+}
+
+const coinGenerator = () => {
+    const coinPool = [10, 10, 10, 10, 10, 10, 10, 50, 50, 50, 50, 50, , 100, 100, 100, 100];
+    let n = Math.floor(Math.random() * coinPool.length) + 1;
+    return coinPool[n];
 }
 
 module.exports = {
