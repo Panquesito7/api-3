a5a3f11c67a993d3652715a478eda17092168e79
Souvik <souvikde.ns@gmail.com>
2021-02-03 13:21:41 +0530

    Update bank.js

--- src/models/bank/bank.js
+++ src/models/bank/bank.js
@@ -1,4 +1,4 @@
-
+const { bankModel } = require('./schema');
 
 class Bank {
     constructor({ id, userId, balance, lastMined }) {
@@ -6,6 +6,25 @@ class Bank {
         this.user_id = userId;
         this.balance = balance;
         this.last_mined = lastMined;
+    }
+    async updateBalance(amount, operation) {
+        let newBalance = null;
+        if(operation === "credit"){
+            newBalance = this.balance + amount;
+        }
+        if(operation === "debit"){
+            if(amount > this.balance){
+                throw "Insufficient balance for transaction";
+            }
+            newBalance = this.balance - amount;
+        }
+
+        try {
+            let res = await bankModel.findByIdAndUpdate(this.user_id, {balance: newBalance});
+            this.balance = newBalance;
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
